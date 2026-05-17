dc73b98b254ca17a396a72ccb73f5f079a92d5c7
minortex <texsd.tt29@outlook.com>
2026-05-17 14:05:56 +0800

    chore: build script

--- build-script.sh
+++ build-script.sh
@@ -1,6 +1,6 @@
 export ANDROID_HOME=$HOME/Android/Sdk
 export ANDROID_SDK_ROOT=$ANDROID_HOME
-export JAVA_HOME=$HOME/Android/Sdk/jdks/jdk11
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk
 export PATH=$JAVA_HOME/bin:$ANDROID_HOME/platform-tools:$ANDROID_HOME/cmdline-tools/latest/bin:$PATH
 
 sh ./gradlew :app:assembleRelease
